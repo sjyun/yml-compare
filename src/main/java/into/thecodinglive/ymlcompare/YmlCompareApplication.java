26b9336838e1ae034fe9c729fa6de6358e36540f
sukjin-yun <sukjin.yun@navercorp.com>
2024-01-18 02:41:51 +0900

    init

---
+++ src/main/java/into/thecodinglive/ymlcompare/YmlCompareApplication.java
@@ -0,0 +1,13 @@
+package into.thecodinglive.ymlcompare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YmlCompareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YmlCompareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/into/thecodinglive/ymlcompare/YmlCompareApplicationTests.java
@@ -0,0 +1,13 @@
+package into.thecodinglive.ymlcompare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YmlCompareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
